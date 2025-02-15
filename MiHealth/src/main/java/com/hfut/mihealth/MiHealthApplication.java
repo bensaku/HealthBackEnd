224738d0795e3435946fff23724e55453089be62
bensaku <2504907080@qq.com>
2025-02-15 14:47:49 +0800

    [task]项目创建

---
+++ MiHealth/src/main/java/com/hfut/mihealth/MiHealthApplication.java
@@ -0,0 +1,13 @@
+package com.hfut.mihealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiHealthApplication.class, args);
+    }
+
+}
---
+++ MiHealth/src/test/java/com/hfut/mihealth/MiHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfut.mihealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
