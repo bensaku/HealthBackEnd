2f21feb9e4ae8e9ca02f54ee2ee324a0809847c0
bensaku <2504907080@qq.com>
2025-04-22 18:39:36 +0800

    [task]ai识别功能完成

--- MiHealth/src/main/java/com/hfut/mihealth/mapper/ImageMapper.java
+++ MiHealth/src/main/java/com/hfut/mihealth/mapper/ImageMapper.java
@@ -9,9 +9,4 @@ import org.springframework.data.repository.query.Param;
 
 @Mapper
 public interface ImageMapper extends BaseMapper<Image> {
-    // 可以根据需要在此处定义更多的SQL语句方法
-    @Insert("INSERT INTO image(user_id, date, timestamp, amount, completed, food_name) " +
-            "VALUES(#{userId}, #{date}, #{timestamp}, #{amount}, #{completed}, #{foodName})")
-    @Options(useGeneratedKeys = true, keyProperty = "imageId", keyColumn = "image_id")
-    void insertImage(@Param("image") Image image);
 }
